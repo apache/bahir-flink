2133e947be13fab05850eed9226799cc40ccf8a1
Leon <32157576+1p4pk@users.noreply.github.com>
2021-02-03 10:20:31 +0100

    Add timestamp of dataPoint to output. (#9)

--- flink-next-gen-connector-influxdb/src/main/java/org/apache/flink/streaming/connectors/influxdb/source/reader/InfluxDBRecordEmitter.java
+++ flink-next-gen-connector-influxdb/src/main/java/org/apache/flink/streaming/connectors/influxdb/source/reader/InfluxDBRecordEmitter.java
@@ -34,6 +34,8 @@ public class InfluxDBRecordEmitter<T> implements RecordEmitter<DataPoint, T, Inf
     public void emitRecord(
             final DataPoint element, final SourceOutput<T> output, final InfluxDBSplit splitState)
             throws Exception {
-        output.collect(this.dataPointDeserializer.deserialize(element));
+        output.collect(this.dataPointDeserializer.deserialize(element), (Long) element.getTime());
+        // TODO: think about splitState timestamp
+        // IDEA: -> splitState.lastTimestamp = Math.max(this.lastTimestamp, element.getTime());
     }
 }
